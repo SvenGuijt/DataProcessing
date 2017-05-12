f98288e272cc2d48a7d1ebb635a45d30c93628e3
Sven Guijt <svenguijt@wcw-stud-145-109-40-52.wireless.uva.nl>
2017-05-12 17:43:54 +0200

    Handing in week 5

--- Lab/week-5/CSV2JSONweek5.py
+++ Lab/week-5/CSV2JSONweek5.py
@@ -15,62 +15,26 @@ def main():
 		# Read in csv file
 		datareader = csv.reader(csvfile, delimiter=",")
 
-		counter = 1
-		blankData = {}
-		blankData["city1"] = "Hupsel"
-		blankData["city2"] = "Beerta"
-		blankData["values1"] = {}
-		blankData["values2"] = {}
-
+		counter = 0
+		Data = []
 
 		# Iterate over each row in datafile
 		for row in datareader:
 
-			currentValue = "value"+str(counter)
+			Data.append({})
 
-			if row[0] == blankData["city1"]:
-				blankData["values1"][currentValue] = {}
-				blankData["values1"][currentValue]["date"] = row[1]
-				blankData["values1"][currentValue]["HumidMin"] = row[4]
-				blankData["values1"][currentValue]["HumidMax"] = row[3]
-				blankData["values1"][currentValue]["HumidMean"] = row[2]
-			else:
-				counter = 1
-				currentValue = "value"+str(counter)
-				blankData["values2"][currentValue] = {}
-				blankData["values2"][currentValue]["date"] = row[1]
-				blankData["values2"][currentValue]["HumidMin"] = row[4]
-				blankData["values2"][currentValue]["HumidMax"] = row[3]
-				blankData["values2"][currentValue]["HumidMean"] = row[2]
+			Data[counter]["place"] = row[0]
+			Data[counter]["date"] = row[1]
+			Data[counter]["humidMin"] = row[4]
+			Data[counter]["humidMax"] = row[3]
+			Data[counter]["humidMean"] = row[2]
 
 			counter += 1
 
 
-
-# Data to look like this:
-
-#		"city1": "Moergestel",
-#		"values1": {
-#		value1: {"date": 20160101, "vochtGem": 95, "vochtMin": 90, "vochtMin": 85}
-#		value2: {"date": 20160102, "vochtGem": 95, "vochtMin": 90, "vochtMin": 85}
-#		value3: {"date": 20160103, "vochtGem": 95, "vochtMin": 90, "vochtMin": 85}
-#		value4: {"date": 20160104, "vochtGem": 95, "vochtMin": 90, "vochtMin": 85}
-#		}
-#		"city2": "Oisterwijk",
-#		"values2": {
-#		value1: {"date": 20160101, "vochtGem": 95, "vochtMin": 90, "vochtMin": 85}
-#		value2: {"date": 20160102, "vochtGem": 95, "vochtMin": 90, "vochtMin": 85}
-#		value3: {"date": 20160103, "vochtGem": 95, "vochtMin": 90, "vochtMin": 85}
-#		value4: {"date": 20160104, "vochtGem": 95, "vochtMin": 90, "vochtMin": 85}
-#		}
-
-
-
-
-
 	# Create json file and write data to it
 	with open("JSONDATA.json", 'w') as OutputFile:
-		json.dump(blankData, OutputFile)
+		json.dump(Data, OutputFile)
 
 
 if __name__ == '__main__':
---
+++ Lab/week-5/multiline.js
@@ -0,0 +1,190 @@
+//Sven Guijt, studentnummer 10597751
+//Data Processing, Minor Programmeren, UvA
+
+// Define svg element and set margins
+var svg = d3.select("svg"),
+    margin = {top: 20, right: 80, bottom: 30, left: 50},
+    width = svg.attr("width") - margin.left - margin.right,
+    height = svg.attr("height") - margin.top - margin.bottom,
+    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Create variable to parse time
+var parseTime = d3.timeParse("%Y%m%d");
+
+// Create scales for x,y axes
+var x = d3.scaleTime().range([0, width]),
+    y = d3.scaleLinear().range([height, 0]),
+    z = d3.scaleOrdinal(d3.schemeCategory10);
+
+// Set line drawing function
+var line = d3.line()
+    .curve(d3.curveBasis)
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.humidMean); });
+
+// Attributes for drawing circles
+var circleAttrs = {
+	  cx: function(d) { return xScale(d.x); },
+	  cy: function(d) { return yScale(d.y); },
+	  r: 5
+	};
+
+// Read in data from json data file
+d3.json("JSONDATAweek5.json", type, function(error, data) {
+  if (error) throw error;
+
+// This variable contains a mapping of the data
+  var humidities = data.slice(1).map(function(id) {
+    return {
+      id: id,
+      values: data.map(function(d) {
+        return {date: d.date, humidity: d[id]};
+      })
+    };
+  });
+
+// Set the x domain attribute
+  x.domain(d3.extent(data, function(d) { return d.date; }));
+
+// Set the y domain attribute
+  y.domain([
+    d3.min(humidities, function(c) { return d3.min(c.values, function(d) { return d.humidMin; }); }),
+    d3.max(humidities, function(c) { return d3.max(c.values, function(d) { return d.humidMax; }); })
+  ]);
+
+// Set the z domain attribute
+  z.domain(humidities.map(function(c) { return c.id; }));
+
+// Create x axis
+  g.append("g")
+      .attr("class", "axis axis--x")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+// Create y axis
+  g.append("g")
+      .attr("class", "axis axis--y")
+      .call(d3.axisLeft(y))
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.69em")
+      .attr("fill", "#000")
+      .text("Humidity, %");
+
+  var humidity = g.selectAll(".humidity")
+    .data(humidities)
+    .enter().append("g")
+      .attr("class", "humidity");
+
+  humidity.append("path")
+      .attr("class", "line")
+      .attr("d", function(d) { return line(d.humidMean); })
+      .style("stroke", function(d) { return z(d.id); });
+
+  humidity.append("path")
+      .attr("class", "line")
+      .attr("d", function(d) { return line(d.humidMin); })
+      .style("stroke", function(d) { return z(d.id); });
+
+  humidity.append("path")
+      .attr("class", "line")
+      .attr("d", function(d) { return line(d.humidMax); })
+      .style("stroke", function(d) { return z(d.id); });
+
+  humidity.append("text")
+      .datum(function(d) { return {id: d.id, value: d.humidMean[d.humidMean.length - 1]}; })
+      .attr("transform", function(d) { return "translate(" + x(d.humidMean.date) + "," + y(d.value.humidMean) + ")"; })
+      .attr("x", 3)
+      .attr("dy", "0.35em")
+      .style("font", "10px sans-serif")
+      .text(function(d) { return d.id; });
+
+
+      // Create circles for mouse events
+	svg.selectAll("circle")
+		.data(data)
+		.enter()
+		.append("circle")
+		.attr(circleAttrs)
+		.on("mouseover", handleMouseOver)
+		.on("mouseout", handleMouseOut);
+
+		// Define mouse events for interactivity
+  function handleMouseOver(d, i) {
+
+        d3.select(this).attr({
+          fill: "orange",
+          r: radius * 2
+        });
+
+        svg.append("text").attr({
+           id: "t" + d.date + "-" + d.humidMean + "-" + i,
+            x: function() { return x(d.x) - 30; },
+            y: function() { return y(d.y) - 15; }
+        })
+        .text(function() {
+          return [d.date, d.humidMean];
+        });
+      }
+
+  function handleMouseOut(d, i) {
+        d3.select(this).attr({
+          fill: "black",
+          r: radius
+        });
+        d3.select("#t" + d.date + "-" + d.humidMean + "-" + i).remove();
+      }
+
+  var dot = svg.append("g")
+      .attr("class", "dots")
+    .selectAll(".dot")
+      .data(interpolateData(1800))
+    .enter().append("circle")
+      .attr("class", "dot")
+      .style("fill", function(d) { return colorScale(d); })
+      .call(position)
+      .on("mousedow", function(d, i) {
+      })
+      .on("mouseup", function(d, i) {
+        dot.classed("selected", false);
+        d3.select(this).classed("selected", !d3.select(this).classed("selected"));
+        dragit.trajectory.display(d, i, "selected");
+
+      })
+      .on("mouseenter", function(d, i) {
+        clear_demo();
+        if(dragit.statemachine.current_state == "idle") {
+          dragit.trajectory.display(d, i)
+          dragit.utils.animateTrajectory(dragit.trajectory.display(d, i), dragit.time.current, 1000)
+          d.humidMean.text(d.plaats);
+          dot.style("opacity", .4)
+          d3.select(this).style("opacity", 1)
+          d3.selectAll(".selected").style("opacity", 1)
+        }
+      })
+      .on("mouseleave", function(d, i) {
+        if(dragit.statemachine.current_state == "idle") {
+          d.humidMean.text("");
+          dot.style("opacity", 1);
+        }
+  
+        dragit.trajectory.remove(d, i);
+      })
+      .call(dragit.object.activate)
+
+});
+
+
+function type(d, _, columns) {
+  d.date = parseTime(d.date);
+  for (var i = 1, n = columns.length, c; i < n; ++i) d[c = columns[i]] = +d[c];
+  return d;
+}
+
+function update(v, duration) {
+	dragit.time.current = v || dragit.time.current;
+	displayYear(dragit.time.current)
+	d3.select("#slider-time").property("value", dragit.time.current);
+}
+
